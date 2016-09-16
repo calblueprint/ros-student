85a52eaf67cd130e49f9204806593a76d64f4ff5
Charles Xue <charlesx2013@gmail.com>
2016-09-16 01:25:30 -0700

    Fixing factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :student do
-
+    sequence(:email) { |n| "student#{n}@gmail.com" }
+    password 'password'
   end
-  factory :admin do
 
+  factory :admin do
+    sequence(:email) { |n| "admin#{n}@gmail.com" }
+    password 'password'
   end
 end
