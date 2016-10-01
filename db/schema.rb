c9b8078577c821690f44dc8d5eca616a95a771fd
Charles Xue <charlesx2013@gmail.com>
2016-10-01 03:07:31 -0700

    migrating db

--- db/schema.rb
+++ db/schema.rb
@@ -37,12 +37,6 @@ ActiveRecord::Schema.define(version: 20160930234500) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
-  create_table "sections", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "module_id"
-    t.integer  "position"
-  end
-
   create_table "code_courses", force: :cascade do |t|
     t.integer  "code_id"
     t.integer  "course_id"
@@ -62,6 +56,14 @@ ActiveRecord::Schema.define(version: 20160930234500) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "module_id"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "student_courses", force: :cascade do |t|
@@ -109,5 +111,23 @@ ActiveRecord::Schema.define(version: 20160930234500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
