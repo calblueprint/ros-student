ceae8604be90038281045cc77de0d4a67fb870b4
Charles Xue <charlesx2013@gmail.com>
2016-10-05 20:52:33 -0700

    Proper serialization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user
+    if current_admin
+      AdminSerializer.new(current_admin, root: false).to_json
+    elsif current_student
+      StudentSerializer.new(current_student, root: false).to_json
+    end
+  end
 end
