9626520d78e1e3879da10f9721f4368078d0e865
Charles Xue <charlesx2013@gmail.com>
2016-10-10 17:01:34 -0700

    Fixing webpack config, added correct routing

--- app/assets/javascripts/course/app.js
+++ app/assets/javascripts/course/app.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import { render } from 'react-dom'
 
 import { ReactRoutes } from '../shared/routes'
 
-import CoursePage from './components/StudentDashboard'
+import CoursePage from './components/CoursePage'
 
 render((
   <Router history={browserHistory}>
