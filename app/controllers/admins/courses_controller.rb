8087c89e56eb30e561d732384acaf980b1f0f2f0
Isaiah Yoon <isaiahyoon@berkeley.edu>
2016-10-14 16:06:01 -0700

    Create course edit bundle

--- app/controllers/admins/courses_controller.rb
+++ app/controllers/admins/courses_controller.rb
@@ -1,4 +1,11 @@
 class Admins::CoursesController < Admins::BaseController
+  load_and_authorize_resource
+
+  def create
+    @course = Course.create
+    redirect_to edit_course_path(@course)
+  end
+
   def edit
   end
 
