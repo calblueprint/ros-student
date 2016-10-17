db134b6d92e7277fb6dafd231bcdc514994f0697
Isaiah Yoon <isaiahyoon@berkeley.edu>
2016-10-17 03:33:44 -0700

    Remove extraneous code in controller

--- app/controllers/admins/courses_controller.rb
+++ app/controllers/admins/courses_controller.rb
@@ -1,11 +1,5 @@
 class Admins::CoursesController < Admins::BaseController
-  load_and_authorize_resource
-
-  def new
-  end
-
   def edit
-    
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -129,22 +129,4 @@ ActiveRecord::Schema.define(version: 20161013054501) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
