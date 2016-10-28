dea77127be4703b272de9828196c72c49169b0f9
Charles Xue <charlesx2013@gmail.com>
2016-10-27 19:11:10 -0700

    Adding homepage logo and center styling

--- app/assets/javascripts/authentication/components/StudentLoginPage.js
+++ app/assets/javascripts/authentication/components/StudentLoginPage.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import { RailsRoutes, ReactRoutes } from '../../shared/routes'
-
+import { Images } from '../../utils/image_helpers'
 import LoginForm from './LoginForm'
 
 class StudentLoginPage extends React.Component {
   render() {
     return (
       <div className='flex login-container'>
-        <h1 className='h1'>Student Login Page!</h1>
+        <img className='login-logo' src={Images.rootsLogo} />
+        <h1 className='h2'>Student Login Page!</h1>
         <LoginForm userType='student' action={RailsRoutes.studentsSignInPath()} />
         <Link to={ReactRoutes.studentsSignUpPath()}>Sign up</Link>
         <Link to={ReactRoutes.studentsForgotPasswordPath()}>Forgot password?</Link>
