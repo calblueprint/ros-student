ede28c7d110c65911564527a4c8c1f4e7769f092
Charles Xue <charlesx2013@gmail.com>
2016-10-28 01:20:25 -0700

    Adding admin login styles

--- app/assets/javascripts/authentication/components/AdminLoginPage.js
+++ app/assets/javascripts/authentication/components/AdminLoginPage.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import Link from 'react-router'
+import { Link } from 'react-router'
 
-import { RailsRoutes } from '../../shared/routes'
+import { ReactRoutes, RailsRoutes } from '../../shared/routes'
+import { Images } from '../../utils/image_helpers'
 
 import LoginForm from './LoginForm'
 
 class AdminLoginPage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Admin Login Page!</h1>
-        <LoginForm userType='admin' action={RailsRoutes.adminsSignInPath()} />
+      <div className='flex center center-vertical login-container'>
+        <div className='flex center center-vertical login-modal'>
+          <img className='login-logo' src={Images.rootsLogo} />
+          <h2 className='h2 marginTopBot-xxs'>Admin Login</h2>
+          <LoginForm userType='admin' action={RailsRoutes.adminsSignInPath()} />
+          <Link to={ReactRoutes.adminsForgotPasswordPath()}>Forgot password?</Link>
+        </div>
       </div>
     )
   }
