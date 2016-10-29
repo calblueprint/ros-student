b0829e1101238e8296e5ae02ca09135edd92977a
Isaiah Yoon <isaiahyoon@berkeley.edu>
2016-10-28 17:10:23 -0700

    Add course name/description inline edit

--- app/assets/javascripts/courseEdit/components/CourseEditPage.js
+++ app/assets/javascripts/courseEdit/components/CourseEditPage.js
@@ -3,18 +3,22 @@ import React from 'react'
 import { APIRoutes } from '../../shared/routes'
 import request from '../../shared/requests/request'
 
+import { getInputToParams } from '../../utils/form_helpers'
+
 import SectionEdit from './SectionEdit'
+import InlineEditInput from '../../shared/components/forms/InlineEditInput'
 
 class CourseEditPage extends React.Component {
   constructor(props) {
     super(props)
     this.id = this.props.routeParams.id
+    this.enableEdit = this.enableEdit.bind(this)
     this.state = {
       course: {
         name: '',
         description: '',
         sections: [],
-      },
+      }
     }
     this.getCourse()
   }
@@ -22,11 +26,47 @@ class CourseEditPage extends React.Component {
   getCourse() {
     const path = APIRoutes.getEditCoursePath(this.id)
     request.get(path, (response) => {
-      console.log(response)
       this.setState( { course: response.course_edit })
     }, (error) => {
       console.log('error')
     })
+  }
+
+  enableEdit() {
+    this.setState({ editable: true })
+  }
+
+  updateCourse(params) {
+    const path = APIRoutes.editCoursePath(this.id)
+    request.update(path, params, (response) => {
+      const course = this.state.course
+      if (params.course.name) {
+        course.name = params.course.name
+      } else {
+        course.description = params.course.description
+      }
+      this.setState({ course: course })
+    }, (error) => {
+      console.log(error)
+    })    
+  }
+
+  onBlurName(value) {
+    const params = { 
+      course: {
+        name: value,
+      }
+    }
+    this.updateCourse(params)
+  }
+
+  onBlurDescription(value) {
+    const params = { 
+      course: {
+        description: value,
+      }
+    }
+    this.updateCourse(params)
   }
 
   renderSections() {
@@ -42,7 +82,10 @@ class CourseEditPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Course Edit: {this.state.course.name}</h1>
+        <h1>
+          <InlineEditInput value={this.state.course.name} onBlur={this.onBlurName.bind(this)} />
+          <InlineEditInput value={this.state.course.description} onBlur={this.onBlurDescription.bind(this)} />
+        </h1>
         <ol>{this.renderSections()}</ol>
       </div>
     )
