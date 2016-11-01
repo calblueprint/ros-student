68421a176e3a068ac4fbf6d1d549f2de7f36cc41
Charles Xue <charlesx2013@gmail.com>
2016-11-01 03:14:15 -0700

    Adding photos model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,14 +20,4 @@ class Photo < ActiveRecord::Base
   def url
     image.url
   end
-
-  def self.convert_base64(data)
-    return unless data
-
-    temp_file = Tempfile.new [Devise.friendly_token, "jpg"]
-    temp_file.binmode
-    temp_file.write(Base64.decode64(data))
-
-    ActionDispatch::Http::UploadedFile.new(tempfile: temp_file, filename: "#{Devise.friendly_token}.jpg")
-  end
 end
