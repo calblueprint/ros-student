b518247cbb504944fa54c653293d87f9a972e32d
Charles Xue <charlesx2013@gmail.com>
2016-11-01 03:14:35 -0700

    Adding uploader logic and filter param logging

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -1,19 +1,25 @@
 # encoding: utf-8
 
 class AudioUploader < CarrierWave::Uploader::Base
-  include CarrierWave::Audio
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :mp3 do
-    process :convert => [:mp3]
-
-    def full_filename(for_file)
-      "#{super.chomp(File.extname(super))}.mp3"
-    end
+  def cache_dir
+    "#{Rails.root}/public/tmp/uploads"
   end
+
+  def file_storage?
+    self.class.storage == CarrierWave::Storage::File
+  end
+
+  # version :mp3 do
+  #   process :convert => [:mp3]
+
+  #   def full_filename(for_file)
+  #     "#{super.chomp(File.extname(super))}.mp3"
+  #   end
+  # end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,43 +10,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def image?
+    image_ext = %w(jpg jpeg gif png)
     if file.respond_to?(:extension)
-      extension_white_list.include?(file.extension.downcase)
+      image_ext.include?(file.extension.downcase)
     else
       # Not all Carrierwave storages respond to :extension
       ext = file.path.split(".").last.downcase
-      extension_white_list.include?(ext)
+      image_ext.include?(ext)
     end
   rescue
     false
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
   def file_storage?
     self.class.storage == CarrierWave::Storage::File
   end
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
 end
