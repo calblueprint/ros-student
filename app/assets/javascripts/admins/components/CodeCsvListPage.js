8af4f6ad90ca34a65eae98f580625f71c3ce692c
Ken Chen <lbkchen@gmail.com>
2016-11-06 21:03:44 -0800

    Adding download button on code_csv list page that downloads codes to machine.

--- app/assets/javascripts/admins/components/CodeCsvListPage.js
+++ app/assets/javascripts/admins/components/CodeCsvListPage.js
@@ -8,7 +8,6 @@ import { getInputToParams } from '../../utils/form_helpers'
 import { APIRoutes } from '../../shared/routes'
 
 import CodeCsvCard from './CodeCsvCard.js'
-import GenerateCodeCsvModal from './GenerateCodeCsvModal.js'
 
 class CodeCsvListPage extends React.Component {
   constructor(props) {
@@ -17,7 +16,6 @@ class CodeCsvListPage extends React.Component {
       code_csvs: []
     }
     this.getCodeCsvs()
-    this.updateCodeCsvs = this.updateCodeCsvs.bind(this)
   }
 
   getCodeCsvs() {
@@ -30,34 +28,26 @@ class CodeCsvListPage extends React.Component {
     })
   }
 
-  updateCodeCsvs(code_csv) {
-    var new_code_csvs = this.state.code_csvs
-    new_code_csvs.push(code_csv)
-    this.setState({ code_csvs: new_code_csvs })
-  }
-
   showCodeCsvGenerateModal(e) {
     e.preventDefault()
   }
 
+  //done
   renderCodeCsvs() {
-    var cc = this.state.code_csvs
-    return Array.from({length: cc.length}, (v, k) => k).map((index) => {
+    return this.state.code_csvs.map((value) => {
       return (
         <li>
-          <CodeCsvCard code_csv={cc[cc.length-index-1]} />
+          <CodeCsvCard code_csv={value}/>
         </li>
       )
     })
   }
 
+  //done
   render() {
-    // <button onClick={this.showCodeCsvGenerateModal}> Generate new codes </button>
-    // NOTE: Add button later when modal is ready
     return (
       <div>
-        <GenerateCodeCsvModal update={this.updateCodeCsvs}/>
-        <h1>List of Codes</h1>
+        <button onClick={this.showCodeCsvGenerateModal}> Generate new codes </button>
         <ul>{this.renderCodeCsvs()}</ul>
       </div>
     )
