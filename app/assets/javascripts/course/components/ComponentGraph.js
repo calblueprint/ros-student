8ca775350bbf94eeae2276c4a72eb2b0bab3bd7f
micahcarroll <micah.d.carroll@gmail.com>
2016-11-17 20:17:55 -0800

    added next/previous buttons

---
+++ app/assets/javascripts/course/components/ComponentGraph.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import _ from 'underscore'
+
+import request from '../../shared/requests/request'
+import { RailsRoutes, ReactRoutes, APIRoutes } from '../../shared/routes'
+
+class ComponentGraph extends React.Component {
+
+  renderComponents() {
+    if (_.isEmpty(this.props.subsection)) {
+      return 'No subsection selected'
+    } else if (_.isEmpty(this.props.subsection.components)) {
+      return 'Loading'
+    } else {
+      return this.props.subsection.components.map((value) => {
+        return <span key={value.id} onClick={_.partial(this.props.callback, value.id)}>{value.id} component {value.component_type} - </span>
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Component Graph</h2>
+        <div>{this.renderComponents()}</div>
+      </div>
+    )
+  }
+}
+
+export default ComponentGraph
