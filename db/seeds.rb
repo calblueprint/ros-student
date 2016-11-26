dc0f701f65d005b09c4c05d22d9ad4132c315533
Charles Xue <charlesx2013@gmail.com>
2016-11-25 23:11:46 -0800

    Adding new form seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,11 +53,47 @@ def create_courses
   end.flatten
 
   subsections.map do |subsection|
-    3.times.map do |n|
-      subsection.components.create component_type: 0,
-                                   content_url: 'https://docs.google.com/presentation/d/1Bi_nOT9xuBrb07dquBM6IZZJ3iTOJmj15adkF3IVqFw/pub?start=false&loop=false&delayms=3000'
+    number = rand(3)
+    case number
+      when 0
+        create_slide_subsection(subsection)
+      when 1
+        create_form_subsection(subsection)
+      when 2
+        create_video_subsection(subsection)
     end
   end
+end
+
+def create_slide_subsection(subsection)
+  10.times do |n|
+    component = subsection.components.create component_type: 0,
+                                             audio: 'http://www.noiseaddicts.com/samples_1w72b820/272.mp3',
+                                             title: FFaker::Education.degree
+    photo = get_photo(component, 'http://image.slidesharecdn.com/slidepresentationguidelines-090612035201-phpapp01/95/slide-presentation-guidelines-1-728.jpg')
+    component.photo = photo
+    component.save
+  end
+end
+
+def create_form_subsection(subsection)
+  subsection.components.create component_type: 1,
+                               content_url: 'https://docs.google.com/forms/d/e/1FAIpQLSdRvdxwyWVVcYwd1Ga6u7nnuBNi3BP6yOSTwGsYlZBsZ5iYag/viewform',
+                               title: FFaker::Education.degree,
+                               form_key: 'water '
+end
+
+def create_video_subsection(subsection)
+  subsection.components.create component_type: 2,
+                               content_url: 'https://www.youtube.com/watch?v=5MgBikgcWnY',
+                               title: FFaker::Education.degree
+end
+
+def get_photo(object, image_url)
+  Photo.create!(
+    parent: object,
+    image: open(image_url),
+  )
 end
 
 create_courses
