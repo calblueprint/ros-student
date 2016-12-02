d8574747b0a8aa1d7e66ecc119c50ddae0d44854
Charles Xue <charlesx2013@gmail.com>
2016-12-02 12:46:54 -0800

    Some refactoring

--- app/assets/javascripts/course/components/CoursePage.js
+++ app/assets/javascripts/course/components/CoursePage.js
@@ -10,7 +10,6 @@ import { APIRoutes } from '../../shared/routes'
 
 
 class CoursePage extends React.Component {
-
   constructor(props) {
     super(props)
 
@@ -51,18 +50,19 @@ class CoursePage extends React.Component {
   }
 
   displayComponent(id) {
-    function byId(element) {
-      return element.id === id
-    }
+    const displayedComponent =
+      this.state.displayedSubsection.components.find((element) => {
+        return element.id === id
+      })
     this.setState({
-      displayedComponent: this.state.displayedSubsection.components.find(byId)
+      displayedComponent: displayedComponent
     })
   }
 
   displayNextComponent() {
-    var components = this.state.displayedSubsection.components;
+    const components = this.state.displayedSubsection.components;
 
-    var index = components.indexOf(this.state.displayedComponent)
+    const index = components.indexOf(this.state.displayedComponent)
     if (index + 1 < components.length) {
       this.displayComponent(components[index + 1].id)
     } else {
@@ -71,24 +71,23 @@ class CoursePage extends React.Component {
   }
 
   displayNextSubsection() {
-    var displayedSubsection = this.state.displayedSubsection
-    var subsections = this.getDisplayedSection(displayedSubsection).subsections
-    function next(element, index) {
+    const displayedSubsection = this.state.displayedSubsection
+    const subsections = this.getDisplayedSection(displayedSubsection).subsections
+    const nextSubsection = subsections.find((element, index) => {
       if (index > 0 && displayedSubsection.id == subsections[index - 1].id) {
         return element
       }
-    }
+    })
 
-    var nextSubsection = subsections.find(next)
     if (nextSubsection != null) {
       this.displaySubsection(nextSubsection.id, 0)
     }
   }
 
   displayPrevComponent() {
-    var components = this.state.displayedSubsection.components;
+    const components = this.state.displayedSubsection.components;
 
-    var index = components.indexOf(this.state.displayedComponent)
+    const index = components.indexOf(this.state.displayedComponent)
     if (index - 1 >= 0) {
       this.displayComponent(components[index - 1].id)
     } else {
@@ -97,22 +96,23 @@ class CoursePage extends React.Component {
   }
 
   displayPrevSubsection() {
-    var displayedSubsection = this.state.displayedSubsection
-    var subsections = this.getDisplayedSection(displayedSubsection).subsections
+    const displayedSubsection = this.state.displayedSubsection
+    const subsections = this.getDisplayedSection(displayedSubsection).subsections
+
     function prev(element, index, array) {
       if (index + 1 < array.length && displayedSubsection.id == subsections[index + 1].id) {
         return element
       }
     }
 
-    var prevSubsection = subsections.find(prev)
+    const prevSubsection = subsections.find(prev)
     if (prevSubsection != null) {
       this.displaySubsection(prevSubsection.id, -1)
     }
   }
 
   getDisplayedSection(displayedSubsection) {
-    var sectionId = displayedSubsection.section_id
+    const sectionId = displayedSubsection.section_id
     function byId(element) {
       return element.id === sectionId
     }
@@ -158,13 +158,13 @@ class CoursePage extends React.Component {
             <div className='flex component-next-container'>
               <button
                 className='marginLeft-lg button'
-                onClick={this.displayPrevComponent}>
-                Previous
+                onClick={this.displayNextComponent}>
+                Next
               </button>
               <button
                 className='button'
-                onClick={this.displayNextComponent}>
-                Next
+                onClick={this.displayPrevComponent}>
+                Previous
               </button>
             </div>
           </div>
