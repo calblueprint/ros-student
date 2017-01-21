057c0a498cdf8ce8641a53764ce8f8c9d367d043
Charles Xue <charlesx2013@gmail.com>
2017-01-21 01:31:16 -0800

    Adding mocha chai enzyme, starting react test framework

---
+++ test/javascripts/authentication/components/SignUpPage.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import {expect} from 'chai'
+
+import SignUpPage from '../../../../app/assets/javascripts/authentication/components/SignUpPage'
+
+describe('<SignUpPage />', () => {
+  it('should have an image to display the gravatar', () => {
+    const signUpPage = shallow(<SignUpPage />)
+    expect(true).to.be.true
+  })
+
+  it('should have props for email and src', () => {
+    const signUpPage = shallow(<SignUpPage />)
+    expect(signUpPage.props().email).to.be.defined
+    expect(signUpPage.props().src).to.be.defined
+  })
+})
