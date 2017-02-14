60faf7c8210c62199a654a9962288fe1ce7bf0fe
Ken Chen <lbkchen@gmail.com>
2017-02-13 18:16:30 -0800

    Fixes rendering of student info when modal loads.

--- app/assets/javascripts/admins/components/StudentProgressModal.js
+++ app/assets/javascripts/admins/components/StudentProgressModal.js
@@ -14,14 +14,20 @@ class StudentProgressModal extends React.Component {
     }
   }
 
-  componentDidUpdate() {
-    console.log(this.props.user)
-  }
-
   getDismissStyle() {
     return {
       backgroundImage: `url(${Images.close_modal})`,
     }
+  }
+
+  getStudentCourses(student) {
+    return student.courses.map((course) => {
+      return (
+        <li>
+          {course.name}
+        </li>
+      )
+    })
   }
 
   render() {
@@ -30,20 +36,28 @@ class StudentProgressModal extends React.Component {
         show={this.props.isModalOpen}
         onHide={this.props.closeModal}
       >
-        <Modal.Header>
-          <Modal.Title>The</Modal.Title>
-          <Modal.Dismiss
-            className='flex center modal-dismiss-container'
-            onClick={this.props.closeModal}>
-            <div style={this.getDismissStyle()} className='modal-dismiss'/>
-          </Modal.Dismiss>
-        </Modal.Header>
-        <Modal.Body>
+        { this.props.student ?
+          <div>
+            <Modal.Header>
+              <Modal.Title>{this.props.student.username}</Modal.Title>
+              <Modal.Dismiss
+                className='flex center modal-dismiss-container'
+                onClick={this.props.closeModal}>
+                <div style={this.getDismissStyle()} className='modal-dismiss'/>
+              </Modal.Dismiss>
+            </Modal.Header>
+            <Modal.Body>
+              <p>{this.props.student.first_name} {this.props.student.last_name}</p>
+              <p>
+                {this.props.student.email}
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
 
-        </Modal.Body>
-        <Modal.Footer>
-
-        </Modal.Footer>
+            </Modal.Footer>
+          </div>
+          : null
+        }
       </Modal>
     )
   }
