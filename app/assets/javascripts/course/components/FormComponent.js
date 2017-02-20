9615df59098d007932f6198ed1cea9fab5d35a53
Kelsey Lam <kelseylam@berkeley.edu>
2017-02-20 13:32:24 -0800

    Added success to Input.js when input is correct. Flip dropdown arrows. Change FormComponent so the input field reloads for a new component

--- app/assets/javascripts/course/components/FormComponent.js
+++ app/assets/javascripts/course/components/FormComponent.js
@@ -8,11 +8,20 @@ class FormComponent extends React.Component {
 
     this.state = {
       formCode: '',
-      error: ''
+      error: '',
+      success: '',
     }
 
     this.updateCodeState = this.updateCodeState.bind(this)
     this.formSubmit = this.formSubmit.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.props.component != nextProps.component ? this.setState({
+                                                    formCode: '',
+                                                    error: '',
+                                                    success: '',
+                                                  })
   }
 
   updateCodeState(e) {
@@ -25,7 +34,7 @@ class FormComponent extends React.Component {
     if (this.state.formCode == this.props.component.form_key) {
       this.props.onEnd()
       this.setState({
-        error: 'You may now press next to continue'
+        success: 'You may now press next to continue',
       })
     } else {
       this.setState({
@@ -43,22 +52,24 @@ class FormComponent extends React.Component {
         >
           Loading...
         </iframe>
-        <div className='flex vertical marginTopBot-sm'>
+        <div className='flex submit-container marginTopBot-sm'>
           <div className='keyword-container'>
             <Input
               name='keyword'
               placeholder='Enter your secret key here.'
               value={this.state.formCode}
               error={this.state.error}
+              success={this.state.success}
               onChange={this.updateCodeState}
             />
           </div>
-          <button
-            className='button'
-            onClick={this.formSubmit}
-          >
-            Submit
-          </button>
+          <div>
+            <button
+              className='button marginTopBot-xxs'
+              onClick={this.formSubmit}>
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     )
