f1ccdc9eee0a4956223cad3497bca645e735cbe5
Ken Chen <lbkchen@gmail.com>
2017-03-02 00:40:12 -0800

    Adds mixin for keyframe animations and updates license to cut off on lines.

---
+++ app/assets/javascripts/shared/components/widgets/RootsLogo.js
@@ -0,0 +1,19 @@
+import React, { PropTypes } from 'react'
+
+import { Images } from '../../../utils/image_helpers'
+
+class RootsLogo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+    )
+  }
+}
+
+RootsLogo.propTypes = {
+}
+
+export default RootsLogo
