87d315d90878bd0d89ec1915980b4307eb1ccd2e
Ken Chen <lbkchen@gmail.com>
2017-03-03 23:50:39 -0800

    Roots of beauty.

--- app/assets/javascripts/shared/components/widgets/RootsLogo.js
+++ app/assets/javascripts/shared/components/widgets/RootsLogo.js
@@ -17,12 +17,12 @@ class RootsLogo extends React.Component {
         <img className='roots-logo-svg circle' src={Images.circle} />
         <img className='roots-logo-svg book-left' src={Images.bookLeft} />
         <img className='roots-logo-svg book-right' src={Images.bookRight} />
-        <img className='roots-logo-svg' src={Images.bookSpine} />
-        <img className='roots-logo-svg' src={Images.page1} />
-        <img className='roots-logo-svg' src={Images.page2} />
-        <img className='roots-logo-svg' src={Images.page3} />
-        <img className='roots-logo-svg' src={Images.page4} />
-        <img className='roots-logo-svg' src={Images.page5} />
+        <img className='roots-logo-svg book-spine' src={Images.bookSpine} />
+        <img className='roots-logo-svg page1' src={Images.page1} />
+        <img className='roots-logo-svg page2' src={Images.page2} />
+        <img className='roots-logo-svg page3' src={Images.page3} />
+        <img className='roots-logo-svg page4' src={Images.page4} />
+        <img className='roots-logo-svg page5' src={Images.page5} />
       </div>
     )
   }
