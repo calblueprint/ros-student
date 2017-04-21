4bc6be2bf1283baa61904542b3cfec6d220a8421
Jason Bao <jzbao@berkeley.edu>
2017-04-21 12:04:37 -0700

    Styling changed

--- app/assets/javascripts/shared/routes.js
+++ app/assets/javascripts/shared/routes.js
@@ -12,22 +12,22 @@ class ReactRoutes {
   static adminsResetPasswordPath()        { return '/admins/password' }
   static studentsResetPasswordPath()      { return '/students/password' }
   static designPath()                     { return '/design' }
-  static studentsRenderPath()             { return '/students'}
+  static studentsRenderPath()             { return '/students' }
 
   // Shared Flow
   static dashboardPath()                  { return '/dashboard' }
 
   // Admin Flow
-  static adminProfilePath(id)             { return `/admins/${ReactRoutes.getId(id, 'id')}/profile`}
-  static codeCsvListPath()                { return `/admins/code_csvs`}
-  static courseToolsPath()                { return `/admins/courses/tools`}
-  static usersListPath()                  { return `/admins/users`}
+  static adminProfilePath(id)             { return `/admins/${ReactRoutes.getId(id, 'id')}/profile` }
+  static codeCsvListPath()                { return `/admins/code_csvs` }
+  static courseToolsPath()                { return `/admins/courses/tools` }
+  static usersListPath()                  { return `/admins/users` }
   static courseRequestApprovePath()       { return `/admins/requests` }
 
-  // Student Flow
-  static studentProfilePath(id)           { return `/students/${ReactRoutes.getId(id, 'id')}/profile`}
-  static courseOutlinePath(id)            { return `/courses/${ReactRoutes.getId(id, 'id')}/outline`}
-  static addCoursesPath()                 {  return `/students/add_courses`}
+  // Student Flow 
+  static studentProfilePath(id)           { return `/students/${ReactRoutes.getId(id, 'id')}/profile` }
+  static courseOutlinePath(id)            { return `/courses/${ReactRoutes.getId(id, 'id')}/outline` }
+  static addCoursesPath()                 {  return `/students/add_courses` }
   static courseRequestPath()              { return `/students/requests` }
 
   // Course Flow
@@ -58,13 +58,13 @@ class RailsRoutes {
 
   // Student
   static coursePath(id)               { return `/students/courses/${id}` }
-  static courseOutlinePath(id)        { return `/courses/${id}/outline`}
+  static courseOutlinePath(id)        { return `/courses/${id}/outline` }
 
   // Admin
   static courseCreatePath()           { return '/courses' }
   static courseEditPath(id)           { return `/courses/${id}/edit` }
-  static adminProfilePath(id)         { return `/admins/${ReactRoutes.getId(id, 'id')}/profile`}
-  static codeCsvListPath()            { return `/admins/code_csvs`}
+  static adminProfilePath(id)         { return `/admins/${ReactRoutes.getId(id, 'id')}/profile` }
+  static codeCsvListPath()            { return `/admins/code_csvs` }
   static courseRequestApprovePath()   { return `/admins/requests` }
 }
 
@@ -83,12 +83,12 @@ class APIRoutes {
   static getStudentCourseSidebarPath(id)  { return APIRoutes.createRoute(`students/courses/${id}/sidebar`) }
   static getStudentCoursesPath()          { return APIRoutes.createRoute(`students/courses`) }
   static getPublishedCourses()            { return APIRoutes.createRoute(`students/courses?is_published=true`) }
-  static addCoursesPath()                 { return APIRoutes.createRoute(`students/codes/add_courses`)}
+  static addCoursesPath()                 { return APIRoutes.createRoute(`students/codes/add_courses`) }
   // FIXME: Refactor to function taking optional params dictionary and converts it to url endpoint
 
   // Admins
-  static importCoursePath()    { return APIRoutes.createRoute(`admins/courses/import`)}
-  static exportCoursePath(id)  { return APIRoutes.createRoute(`admins/courses/${id}/export`)}
+  static importCoursePath()    { return APIRoutes.createRoute(`admins/courses/import`) }
+  static exportCoursePath(id)  { return APIRoutes.createRoute(`admins/courses/${id}/export`) }
   static createAdminPath()     { return APIRoutes.createRoute(`admins`) }
   static getStudentsPath()     { return APIRoutes.createRoute(`students`) }
   static getAdminsPath()       { return APIRoutes.createRoute(`admins`) }
@@ -104,22 +104,22 @@ class APIRoutes {
   // Codes
   static codeCsvListPath()       { return APIRoutes.createRoute(`admins/code_csvs`) }
   static codeCsvDownloadPath(id) { return APIRoutes.createRoute(`admins/code_csvs/${id}/download.csv`) }
-  static getCodeCsvPath(id)      { return APIRoutes.createRoute(`admins/code_csvs/${id}`)}
+  static getCodeCsvPath(id)      { return APIRoutes.createRoute(`admins/code_csvs/${id}`) }
 
   // Courses
   static getSubsectionPath(id)            { return APIRoutes.createRoute(`students/subsections/${id}`) }
   static getComponentProgressPath(id)     { return APIRoutes.createRoute(`students/components/${id}/component_progresses`) }
-  static createComponentProgressPath(id)  { return APIRoutes.createRoute(`students/components/${id}/component_progresses`)}
+  static createComponentProgressPath(id)  { return APIRoutes.createRoute(`students/components/${id}/component_progresses`) }
 
   //Student Courses
-  static finishedCoursePath(id)           { return APIRoutes.createRoute(`students/student_courses/${id}`)}
-  static getStudentCourseIdPath(id)       { return APIRoutes.createRoute(`students/student_courses/${id}`)}
+  static finishedCoursePath(id)           { return APIRoutes.createRoute(`students/student_courses/${id}`) }
+  static getStudentCourseIdPath(id)       { return APIRoutes.createRoute(`students/student_courses/${id}`) }
 
   //Requests
   static courseRequestPath()              { return APIRoutes.createRoute(`students/requests`) }
   static requestUpdatePath(id)            { return APIRoutes.createRoute(`admins/requests/${id}`) }
-  static getRequestsPath()                { return APIRoutes.createRoute(`admins/requests`)}
-  static getIncompleteRequestsPath()      { return APIRoutes.createRoute(`admins/requests?by_state=0`)}
+  static getRequestsPath()                { return APIRoutes.createRoute(`admins/requests`) }
+  static getIncompleteRequestsPath()      { return APIRoutes.createRoute(`admins/requests?by_state=0`) }
 
   // Course Edit
   static createComponentPath(id)    { return APIRoutes.createRoute(`admins/subsections/${id}/components`) }
@@ -134,10 +134,10 @@ class APIRoutes {
   static reorderSectionPath(id)     { return APIRoutes.createRoute(`admins/sections/${id}/switch_position`) }
   static reorderSubsectionPath(id)  { return APIRoutes.createRoute(`admins/subsections/${id}/switch_position`) }
   static reorderComponentPath(id)   { return APIRoutes.createRoute(`admins/components/${id}/switch_position`) }
-  static getSectionsPath(id)        { return APIRoutes.createRoute( `admins/courses/${id}/sections`)}
-  static getSubsectionsPath(id)     { return APIRoutes.createRoute( `admins/sections/${id}/subsections`)}
-  static switchSubsectionPath(id)   { return APIRoutes.createRoute( `admins/components/${id}/switch_subsection`)}
-  static switchSectionPath(id)      { return APIRoutes.createRoute( `admins/subsections/${id}/switch_section`)}
+  static getSectionsPath(id)        { return APIRoutes.createRoute(`admins/courses/${id}/sections`) }
+  static getSubsectionsPath(id)     { return APIRoutes.createRoute(`admins/sections/${id}/subsections`) }
+  static switchSubsectionPath(id)   { return APIRoutes.createRoute(`admins/components/${id}/switch_subsection`) }
+  static switchSectionPath(id)      { return APIRoutes.createRoute(`admins/subsections/${id}/switch_section`) }
 }
 
 export {
