6dbaf328a7ab4c758dbf19d74558fbf7423360bd
Charles Xue <charlesx2013@gmail.com>
2017-04-24 01:08:45 -0700

    Fixing position error

--- app/controllers/api/admins/components_controller.rb
+++ app/controllers/api/admins/components_controller.rb
@@ -31,7 +31,7 @@ class Api::Admins::ComponentsController < Api::Admins::BaseController
 
   def switch_position
     if @component.switch(switch_position_params)
-      render json: @component, serializer: ComponentAdminSerializer, root: false
+      render json: @component.subsection, serializer: SubsectionAdminSerializer, root: false
     else
       error_response(nil, 'Invalid position given')
     end
